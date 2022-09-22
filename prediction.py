1b0e149d55809565d3ec4cde7c45e145bba5675a
Teruto Endo <endo2014.17@gmail.com>
2022-09-22 16:17:43 +0900

    ラムダランク用の予測コードを追加

--- prediction.py
+++ prediction.py
@@ -21,10 +21,26 @@ load_dotenv(dotenv_path)
 GOOGLE_DRIVE_PATH = os.environ.get("GOOGLE_DRIVE_PATH")
 TRAIN_DATA_PATH = GOOGLE_DRIVE_PATH + "/train_data"
 
-def predict_using_lambdarank(target_data_path):
-    df_for_prediction = dataset.load_df_for_prediction()
+def predict_using_lambdarank(target_data_path, file_path):
+    df_for_prediction = dataset.load_df_for_prediction_of_lambdarank(target_data_path)
+    model = pickle.load(open(file_path, 'rb'))
+    x = np.array(df_for_prediction)
+    pred = model.predict(x, num_iteration=model.best_iteration)
     
+    res = pd.DataFrame(columns=['horse_id', 'horse_number', 'score'])
+    for score, horse_number, horse_id in zip(pred, list(df_for_prediction["horse_number"]), list(df_for_prediction["horse_id"])):
+        res = res.append(pd.DataFrame([[horse_id, horse_number, -score]], columns=['horse_id', 'horse_number', 'score']))
+        
+    def softmax(a):
+        x = np.exp(a)
+        u = np.sum(x)
+        return x/u
+    res["proc"] = softmax(np.array(res["score"]))
+    
+    print(sum(res.proc))
+    print(res.sort_values('score', ascending=False))
 
 if __name__ == "__main__":
     target_data_path = GOOGLE_DRIVE_PATH + '/test_data/takarazuka/'
+    file_path = "./model_data/lambdarank/lgb_model.pkl"
     predict_using_lambdarank(target_data_path)
